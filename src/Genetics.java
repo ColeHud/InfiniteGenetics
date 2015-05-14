a44fdb7efdbf42aa6779c694feb8221e7790a454
ColeHud <cmhudson11@gmail.com>
2015-05-13 21:35:04 -0400

    User input

--- src/Genetics.java
+++ src/Genetics.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Genetics 
@@ -295,5 +296,72 @@ public class Genetics
 			
 		}
 	}
+
+	//user loop
+	public void userLoop()
+	{
+		System.out.println("Infinite Genetics: Infinitely large Punnett squares");
+		System.out.println();
+		
+		//loop
+		while(true)
+		{
+			//get user input, and check if it is correct
+			boolean correctInput = false;
+			String parentOneString = "";
+			String parentTwoString = "";
+			Scanner in = new Scanner(System.in);
+			while(!correctInput)
+			{
+				parentOneString = "";
+			    parentTwoString = "  ";
+				System.out.print("Enter the genotype of parent 1 (e.g. BbLL): ");
+				parentOneString = in.next();
+				
+				System.out.print("Enter the genotype of parent 2: ");
+				parentTwoString = in.next();
+				
+				if(parentOneString.length() == parentTwoString.length())
+				{
+					correctInput = true;
+				}
+				else
+				{
+					System.out.println("==============================================");
+					System.out.println("The two genotypes need to be of the same length");
+					System.out.println("==============================================");
+					
+				}
+			}
+			
+			//the input was correct
+			this.parent1 = parentOneString;
+			this.parent2 = parentTwoString;
+			
+			Genetics genetics = new Genetics(parent1, parent2);
+			
+			//perform methods, conversion, and other stuff
+			genetics.convertParent1StringToArray();
+			genetics.convertParent2StringToArray();
+			genetics.newCombosOne();
+			genetics.newCombosTwo();
+			
+			//print out
+			System.out.println();
+			genetics.printResults();
+			System.out.println();
+			
+			//check if the user wants to go again
+			System.out.print("Do you want to go again? (y/n): ");
+			String goAgain = in.next().toLowerCase();
+			
+			if(!goAgain.equals("y"))
+			{
+				break;
+			}
+			
+		}
+	}
+	
 
 }
--- src/GeneticsTester.java
+++ src/GeneticsTester.java
@@ -1,28 +1,84 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class GeneticsTester 
 {
 	public static void main(String[] args)
 	{
-		//create variables
-		Genetics genetics = new Genetics("FfGgXxRr", "FFggXXrr");
-		genetics.convertParent1StringToArray();
-		genetics.convertParent2StringToArray();
-		genetics.newCombosOne();
-		genetics.newCombosTwo();
-		
+		Genetics genetics = new Genetics();
 
-		//ArrayList<String> array = genetics.newCombosOne();
-		
-		genetics.results();
-		genetics.printResults();
-		
-		
-		//get user input
-		
-		//perform calculations
-		
-		//print results
+		userLoop();
+	}
+
+	//user loop
+	public static void userLoop()
+	{
+		System.out.println("Infinite Genetics: Infinitely large Punnett squares");
+		System.out.println();
+
+		//loop
+		while(true)
+		{
+			//get user input, and check if it is correct
+			boolean correctInput = false;
+			String parentOneString = "";
+			String parentTwoString = "";
+			Scanner in = new Scanner(System.in);
+			while(!correctInput)
+			{
+				parentOneString = "";
+				parentTwoString = "  ";
+				System.out.print("Enter the genotype of parent 1 (e.g. BbLL): ");
+				parentOneString = in.next();
+
+				System.out.print("Enter the genotype of parent 2: ");
+				parentTwoString = in.next();
+
+				if(parentOneString.length() == parentTwoString.length())
+				{
+					correctInput = true;
+				}
+				else
+				{
+					System.out.println("==============================================");
+					System.out.println("The two genotypes need to be of the same length");
+					System.out.println("==============================================");
+
+				}
+			}
+
+			//the input was correct
+			Genetics genetics = new Genetics();
+			
+			genetics.setParent1(parentOneString);
+			genetics.setParent2(parentTwoString);
+
+			
+
+			//perform methods, conversion, and other stuff
+			genetics.convertParent1StringToArray();
+			genetics.convertParent2StringToArray();
+			genetics.newCombosOne();
+			genetics.newCombosTwo();
+			genetics.results();
+
+			//print out
+			System.out.println();
+			genetics.printResults();
+			System.out.println();
+
+			//check if the user wants to go again
+			System.out.print("Do you want to go again? (y/n): ");
+			String goAgain = in.next().toLowerCase();
+
+			if(!goAgain.equals("y"))
+			{
+				System.out.println("Bye!!!");
+				break;
+			}
+			System.out.println();
+
+		}
 	}
 }
